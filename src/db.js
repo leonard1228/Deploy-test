f920a04b7b1a17caf8f7ca0ef81bbab6937680f5
Tu Nombre <tuemail@gmail.com>
2026-05-05 18:20:40 -0500

    fix ssl config

--- src/db.js
+++ src/db.js
@@ -2,12 +2,17 @@ import pkg from 'pg';
 const { Pool } = pkg;
 import { DB_CONFIG } from './config.js';
 
-const pool = new Pool(DB_CONFIG);
+const pool = new Pool({
+  ...DB_CONFIG,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  family: 4
+});
 
 pool.on('error', (err) => {
   console.error('Error en el pool de conexiones:', err);
 });
-ssl: {
-  rejectUnauthorized: false
-}
+
+
 export default pool;
